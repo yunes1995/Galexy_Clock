b89f846c630eb8bd16ca4b1c5fe7d600cb49c7c6
Yunes Mohammadi <younesm837@gmail.com>
2022-11-30 02:47:55 -0800

    version 1 (in this version html & CSS & JS is created and is done)

---
+++ script.js
@@ -0,0 +1,450 @@
+function show_time(){
+    var d = new Date();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+    var second = d.getSeconds();
+    var years = d.getFullYear();
+    var month = d.getMonth();
+    var days = d.getDay();
+
+    if(hours<10){hours = "0" + hours};
+    if(minutes<10){ minutes ="0" + minutes};
+    if(second<10){second ="0"+second};
+    if(month<10){month = "0"+ month};
+    if(days<10){days = "0"+ days};
+    // hours
+    document.getElementById("hourShow").innerText = hours;
+    // hours
+
+/*-------------------secound---------------*/
+
+if(second == 1){
+    s1.classList.add("activeSec");
+}
+if(second == 2){
+    s1.classList.remove("activeSec");
+    s2.classList.add("activeSec");
+}
+if(second == 3){
+    s2.classList.remove("activeSec");
+    s3.classList.add("activeSec");
+}
+if(second == 4){
+    s3.classList.remove("activeSec");
+    s4.classList.add("activeSec");
+}
+if(second == 5){
+    s4.classList.remove("activeSec");
+    s5.classList.add("activeSec");
+}
+if(second == 6){
+    s5.classList.remove("activeSec");
+    s6.classList.add("activeSec");
+}if(second == 7){
+    s6.classList.remove("activeSec");
+    s7.classList.add("activeSec");
+}if(second == 8){
+    s7.classList.remove("activeSec");
+    s8.classList.add("activeSec");
+}if(second == 9){
+    s8.classList.remove("activeSec");
+    s9.classList.add("activeSec");
+}if(second == 10){
+    s9.classList.remove("activeSec");
+    s10.classList.add("activeSec");
+}if(second == 11){
+    s10.classList.remove("activeSec");
+    s11.classList.add("activeSec");
+}if(second == 12){
+    s11.classList.remove("activeSec");
+    s12.classList.add("activeSec");
+}if(second == 13){
+    s12.classList.remove("activeSec");
+    s13.classList.add("activeSec");
+}if(second == 14){
+    s13.classList.remove("activeSec");
+    s14.classList.add("activeSec");
+}if(second == 15){
+    s14.classList.remove("activeSec");
+    s15.classList.add("activeSec");
+}if(second == 16){
+    s15.classList.remove("activeSec");
+    s16.classList.add("activeSec");
+}if(second == 17){
+    s16.classList.remove("activeSec");
+    s17.classList.add("activeSec");
+}if(second == 18){
+    s17.classList.remove("activeSec");
+    s18.classList.add("activeSec");
+}if(second == 19){
+    s18.classList.remove("activeSec");
+    s19.classList.add("activeSec");
+}if(second == 20){
+    s19.classList.remove("activeSec");
+    s20.classList.add("activeSec");
+}if(second == 21){
+    s20.classList.remove("activeSec");
+    s21.classList.add("activeSec");
+}if(second == 22){
+    s21.classList.remove("activeSec");
+    s22.classList.add("activeSec");
+}if(second == 23){
+    s22.classList.remove("activeSec");
+    s23.classList.add("activeSec");
+}if(second == 24){
+    s23.classList.remove("activeSec");
+    s24.classList.add("activeSec");
+}if(second == 25){
+    s24.classList.remove("activeSec");
+    s25.classList.add("activeSec");
+}if(second == 26){
+    s25.classList.remove("activeSec");
+    s26.classList.add("activeSec");
+}if(second == 27){
+    s26.classList.remove("activeSec");
+    s27.classList.add("activeSec");
+}if(second == 28){
+    s27.classList.remove("activeSec");
+    s28.classList.add("activeSec");
+}if(second == 29){
+    s28.classList.remove("activeSec");
+    s29.classList.add("activeSec");
+}if(second == 30){
+    s29.classList.remove("activeSec");
+    s30.classList.add("activeSec");
+}if(second == 31){
+    s30.classList.remove("activeSec");
+    s31.classList.add("activeSec");
+}if(second == 32){
+    s31.classList.remove("activeSec");
+    s32.classList.add("activeSec");
+}if(second == 33){
+    s32.classList.remove("activeSec");
+    s33.classList.add("activeSec");
+}if(second == 34){
+    s33.classList.remove("activeSec");
+    s34.classList.add("activeSec");
+}if(second == 35){
+    s34.classList.remove("activeSec");
+    s35.classList.add("activeSec");
+}if(second == 36){
+    s35.classList.remove("activeSec");
+    s36.classList.add("activeSec");
+}if(second == 37){
+    s36.classList.remove("activeSec");
+    s37.classList.add("activeSec");
+}if(second == 38){
+    s37.classList.remove("activeSec");
+    s38.classList.add("activeSec");
+}if(second == 39){
+    s38.classList.remove("activeSec");
+    s39.classList.add("activeSec");
+}if(second == 40){
+    s39.classList.remove("activeSec");
+    s40.classList.add("activeSec");
+}if(second == 41){
+    s40.classList.remove("activeSec");
+    s41.classList.add("activeSec");
+}if(second == 42){
+    s41.classList.remove("activeSec");
+    s42.classList.add("activeSec");
+}if(second == 43){
+    s42.classList.remove("activeSec");
+    s43.classList.add("activeSec");
+}if(second == 44){
+    s43.classList.remove("activeSec");
+    s44.classList.add("activeSec");
+}if(second == 45){
+    s44.classList.remove("activeSec");
+    s45.classList.add("activeSec");
+}if(second == 46){
+    s45.classList.remove("activeSec");
+    s46.classList.add("activeSec");
+}if(second == 47){
+    s46.classList.remove("activeSec");
+    s47.classList.add("activeSec");
+}if(second == 48){
+    s47.classList.remove("activeSec");
+    s48.classList.add("activeSec");
+}if(second == 49){
+    s48.classList.remove("activeSec");
+    s49.classList.add("activeSec");
+}if(second == 50){
+    s49.classList.remove("activeSec");
+    s50.classList.add("activeSec");
+}if(second == 51){
+    s50.classList.remove("activeSec");
+    s51.classList.add("activeSec");
+}if(second == 52){
+    s51.classList.remove("activeSec");
+    s52.classList.add("activeSec");
+}if(second == 53){
+    s52.classList.remove("activeSec");
+    s53.classList.add("activeSec");
+}if(second == 54){
+    s53.classList.remove("activeSec");
+    s54.classList.add("activeSec");
+}if(second == 55){
+    s54.classList.remove("activeSec");
+    s55.classList.add("activeSec");
+}if(second == 56){
+    s55.classList.remove("activeSec");
+    s56.classList.add("activeSec");
+}if(second == 57){
+    s56.classList.remove("activeSec");
+    s57.classList.add("activeSec");
+}if(second == 58){
+    s57.classList.remove("activeSec");
+    s58.classList.add("activeSec");
+}if(second == 59){
+    s58.classList.remove("activeSec");
+    s59.classList.add("activeSec");
+}
+if(second == 60){
+    s59.classList.remove("activeSec");
+    
+}
+/*-------------------secound---------------*/
+
+/*--------------------minutes---------------*/
+
+if(minutes == 1){
+    m1.classList.add("activeMin");
+}
+if(minutes == 2){
+    m1.classList.remove("activeMin")
+    m2.classList.add("activeMin");
+}
+if(minutes == 3){
+    m2.classList.remove("activeMin")
+    m3.classList.add("activeMin");
+}
+if(minutes == 4){
+    m3.classList.remove("activeMin")
+    m4.classList.add("activeMin");
+}
+if(minutes == 5){
+    m4.classList.remove("activeMin")
+    m5.classList.add("activeMin");
+}
+if(minutes == 6){
+    m5.classList.remove("activeMin")
+    m6.classList.add("activeMin");
+}
+if(minutes == 7){
+    m6.classList.remove("activeMin")
+    m7.classList.add("activeMin");
+}
+if(minutes == 8){
+    m7.classList.remove("activeMin")
+    m8.classList.add("activeMin");
+}
+if(minutes == 9){
+    m8.classList.remove("activeMin")
+    m9.classList.add("activeMin");
+}
+if(minutes == 10){
+    m9.classList.remove("activeMin")
+    m10.classList.add("activeMin");
+}
+if(minutes == 11){
+    m10.classList.remove("activeMin")
+    m11.classList.add("activeMin");
+}
+if(minutes == 12){
+    m11.classList.remove("activeMin")
+    m12.classList.add("activeMin");
+}
+if(minutes == 13){
+    m12.classList.remove("activeMin")
+    m13.classList.add("activeMin");
+}
+if(minutes == 14){
+    m13.classList.remove("activeMin")
+    m14.classList.add("activeMin");
+}
+if(minutes == 15){
+    m14.classList.remove("activeMin")
+    m15.classList.add("activeMin");
+}
+if(minutes == 16){
+    m15.classList.remove("activeMin")
+    m16.classList.add("activeMin");
+}
+if(minutes == 17){
+    m16.classList.remove("activeMin")
+    m17.classList.add("activeMin");
+}
+if(minutes ==18){
+    m17.classList.remove("activeMin")
+    m18.classList.add("activeMin");
+}
+if(minutes == 19){
+    m18.classList.remove("activeMin")
+    m19.classList.add("activeMin");
+}
+if(minutes == 20){
+    m19.classList.remove("activeMin")
+    m20.classList.add("activeMin");
+}
+if(minutes == 21){
+    m20.classList.remove("activeMin")
+    m21.classList.add("activeMin");
+}
+if(minutes == 22){
+    m21.classList.remove("activeMin")
+    m22.classList.add("activeMin");
+}
+if(minutes == 23){
+    m22.classList.remove("activeMin")
+    m23.classList.add("activeMin");
+}
+if(minutes == 24){
+    m23.classList.remove("activeMin")
+    m24.classList.add("activeMin");
+}
+if(minutes == 25){
+    m24.classList.remove("activeMin")
+    m25.classList.add("activeMin");
+}
+if(minutes == 26){
+    m25.classList.remove("activeMin")
+    m26.classList.add("activeMin");
+}
+if(minutes == 27){
+    m26.classList.remove("activeMin")
+    m27.classList.add("activeMin");
+}
+if(minutes == 28){
+    m27.classList.remove("activeMin")
+    m28.classList.add("activeMin");
+}
+if(minutes == 29){
+    m28.classList.remove("activeMin")
+    m29.classList.add("activeMin");
+}
+if(minutes == 30){
+    m29.classList.remove("activeMin")
+    m30.classList.add("activeMin");
+}
+if(minutes == 31){
+    m30.classList.remove("activeMin")
+    m31.classList.add("activeMin");
+}
+if(minutes == 32){
+    m31.classList.remove("activeMin")
+    m32.classList.add("activeMin");
+}
+if(minutes == 33){
+    m32.classList.remove("activeMin")
+    m33.classList.add("activeMin");
+}
+if(minutes == 34){
+    m33.classList.remove("activeMin")
+    m34.classList.add("activeMin");
+}
+if(minutes == 35){
+    m34.classList.remove("activeMin")
+    m35.classList.add("activeMin");
+}
+if(minutes == 36){
+    m35.classList.remove("activeMin")
+    m36.classList.add("activeMin");
+}
+if(minutes == 37){
+    m36.classList.remove("activeMin")
+    m37.classList.add("activeMin");
+}
+if(minutes == 38){
+    m37.classList.remove("activeMin")
+    m38.classList.add("activeMin");
+}
+if(minutes == 39){
+    m38.classList.remove("activeMin")
+    m39.classList.add("activeMin");
+}
+if(minutes == 40){
+    m39.classList.remove("activeMin")
+    m40.classList.add("activeMin");
+}
+if(minutes == 41){
+    m40.classList.remove("activeMin")
+    m41.classList.add("activeMin");
+}
+if(minutes == 42){
+    m41.classList.remove("activeMin")
+    m42.classList.add("activeMin");
+}
+if(minutes == 43){
+    m42.classList.remove("activeMin")
+    m43.classList.add("activeMin");
+}
+if(minutes == 44){
+    m43.classList.remove("activeMin")
+    m44.classList.add("activeMin");
+}
+if(minutes == 45){
+    m44.classList.remove("activeMin")
+    m45.classList.add("activeMin");
+}
+if(minutes == 46){
+    m45.classList.remove("activeMin")
+    m46.classList.add("activeMin");
+}
+if(minutes == 47){
+    m46.classList.remove("activeMin")
+    m47.classList.add("activeMin");
+}
+if(minutes == 48){
+    m47.classList.remove("activeMin")
+    m48.classList.add("activeMin");
+}
+if(minutes == 49){
+    m48.classList.remove("activeMin")
+    m49.classList.add("activeMin");
+}
+if(minutes == 50){
+    m49.classList.remove("activeMin")
+    m50.classList.add("activeMin");
+}
+if(minutes == 51){
+    m50.classList.remove("activeMin")
+    m51.classList.add("activeMin");
+}
+if(minutes == 52){
+    m51.classList.remove("activeMin")
+    m52.classList.add("activeMin");
+}
+if(minutes == 53){
+    m52.classList.remove("activeMin")
+    m53.classList.add("activeMin");
+}
+if(minutes == 54){
+    m53.classList.remove("activeMin")
+    m54.classList.add("activeMin");
+}
+if(minutes == 55){
+    m54.classList.remove("activeMin")
+    m55.classList.add("activeMin");
+}
+if(minutes == 56){
+    m55.classList.remove("activeMin")
+    m56.classList.add("activeMin");
+}
+if(minutes == 57){
+    m56.classList.remove("activeMin")
+    m57.classList.add("activeMin");
+}
+if(minutes == 59){
+    m58.classList.remove("activeMin")
+    m59.classList.add("activeMin");
+}
+if(minutes == 60){
+    m59.classList.remove("activeMin");
+}
+
+    setTimeout(show_time,1000);
+}
+
+
+show_time();
